036dc88c6ff5f4e43d51e23415548e99005dd6bf
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-05-27 14:48:17 -0300

    creacion

---
+++ pito.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class pito {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String  pito = scan.nextLine();
+        System.out.println("ola" + pito);
+        scan.close();
+    }
+}
